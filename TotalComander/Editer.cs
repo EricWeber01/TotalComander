3e0368266f6acc7253b780864e4e060d51dbde65
babya <babya@ERIC_WEBER>
2022-06-30 13:49:43 +0300

    Add project files.

---
+++ TotalComander/Editer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TotalComander
+{
+    class Editer
+    {
+        // public string StrAfterEdit { get; set; } = string.Empty;
+        public string EditString(string str)
+        {
+            if (str[str.Length - 1] == '\\')
+            {
+                str = str.Remove(str.Length - 1, 1);
+                while (str[str.Length - 1] != '\\')
+                {
+                    str = str.Remove(str.Length - 1, 1);
+
+                }
+            }
+            else if (str[str.Length - 1] != '\\')
+            {
+                while (str[str.Length - 1] != '\\')
+                {
+                    str = str.Remove(str.Length - 1, 1);
+                }
+            }
+            return str;
+        }
+    }
+}
---
+++ TotalComander/Form2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TotalComander
+{
+    public partial class Form2 : Form
+    {
+        IFormData parentForm;
+        public Form2(IFormData f)
+        {
+            InitializeComponent();
+            parentForm = f;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            parentForm.AddText(textBox1.Text);
+            Close();
+        }
+    }
+}
